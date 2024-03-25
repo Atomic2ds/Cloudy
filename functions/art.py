1babeb017d573b8bba2e6a40001fd399815830af
Atomic2ds <109296824+Atomic2ds@users.noreply.github.com>
2024-03-25 14:52:44 +0800

    Fixed art uploads not working

--- functions/art.py
+++ functions/art.py
@@ -19,10 +19,10 @@ async def upload_art_image(image_url):
   if response.status_code == 200:
       with open("./images/cache/art.jpg", "wb") as f:
           f.write(response.content)
-
+          
   files = {'file': open('./images/cache/art.jpg', 'rb')}
   headers = {'Authorization': f'Bearer {config.MEDIA_AUTH_KEY}'}
-  response = requests.post(f"{libraries.MEDIA_URL}/upload", files=files, headers=headers)
+  response = requests.post(f"{libraries.MEDIA_URL}/api/upload", files=files, headers=headers)
   json_response = response.json()
   filename = json_response.get('filename')
   response_url = f"{libraries.MEDIA_URL}/api/uploads/{filename}"
